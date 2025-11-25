d0b3585219ed27b6cd927432b5791fe1cb4d2863
tariktoplu <tarikttoplu@gmail.com>
2025-11-25 19:31:20 +0300

    re-designed api

--- Web/Controllers/AdminController.cs
+++ Web/Controllers/AdminController.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
 namespace Web.Controllers;
 
 [Authorize(Roles = "Admin")]
 public class AdminController : Controller
 {
-    // Bu sayfa sadece API'yi tüketmek için bir arayüz olacak
-    public IActionResult Reports()
-    {
-        return View();
-    }
+    public IActionResult Reports() => View();
 }
